6de7d7ab6107be698db744bcd51f922ffbc05581
ammoti <vahap.yigit@gmail.com>
2020-10-22 22:55:51 +0300

    Average duration approx. 8.6 seconds

--- InterviewQuestion/Program.cs
+++ InterviewQuestion/Program.cs
@@ -1,8 +1,13 @@
-﻿using InterviewQuestion.Models;
+﻿using InterviewQuestion.Enums;
+using InterviewQuestion.Models;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace InterviewQuestion
 {
@@ -22,14 +27,54 @@ namespace InterviewQuestion
 
             ////////////////////////////////////
             /// Place your answer after this section
-            ///////////////////////////////////
+            ///////////////////////////////////           
 
-            
+            Hashtable hashtable = new Hashtable();
+            for (int i = 0; i < queryElements.Count; i++)
+            {
+                var element = queryElements[i];
+                var getProp = typeof(QueryResultTable).GetProperty("column" + element.Index);
+                hashtable.Add(element.Index, getProp);
 
+            }
+            int value;
+            var group = queryResultTables.GroupBy(x => x.perfdate);
+            Parallel.ForEach(group, new ParallelOptions { MaxDegreeOfParallelism = Convert.ToInt32(Math.Ceiling((Environment.ProcessorCount * 0.75) * 1.0)) },
+                   index =>
+                   {
+                       foreach (var item in queryElements)
+                       {
+                           PropertyInfo colPro = (PropertyInfo)hashtable[item.Index];
+                           switch (item.Aggregate)
+                           {
+                               case AggregateType.sum:
+                                   var total = index.Sum(s => int.TryParse(colPro?.GetValue(s).ToString(), out value) ? value : 0);
+                                   colPro.SetValue(index.First(), total.ToString());
+                                   break;
+
+                               case AggregateType.avg:
+                                   var avg = index.Average(s => int.TryParse(colPro?.GetValue(s).ToString(), out value) ? value : 0);
+                                   colPro.SetValue(index.First(), avg.ToString());
+                                   break;
+
+                               case AggregateType.max:
+                                   var max = index.Max(s => int.TryParse(colPro?.GetValue(s).ToString(), out value) ? value : 0);
+                                   colPro.SetValue(index.First(), max.ToString());
+                                   break;
+
+                               case AggregateType.min:
+                                   var min = index.Min(s => int.TryParse(colPro?.GetValue(s).ToString(), out value) ? value : 0);
+                                   colPro.SetValue(index.First(), min.ToString());
+                                   break;
+                               default:
+                                   break;
+                           }
+                       }
+                   });
             ////////////////////////////////////
             /// Place your answer before this section
             ///////////////////////////////////
-            
+
             // Stop Timer
             stopWatch.Stop();
 
@@ -44,7 +89,7 @@ namespace InterviewQuestion
             ////////////////////////////
             // Data Validation
             ////////////////////////////
-            
+
             if (queryResultTables[0].column1 != "800")
             {
                 Console.WriteLine("ERROR IN CALCULATION for column1!");
